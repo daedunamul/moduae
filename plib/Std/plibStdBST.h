6d88c22d6a84632121e8e55635e09f7c079bb25b
puppysss <49893731+puppysss@users.noreply.github.com>
2019-08-30 15:42:05 +0900

    Update plibStdBST.h

--- plib/Std/plibStdBST.h
+++ plib/Std/plibStdBST.h
@@ -6,7 +6,7 @@
 #pragma once
 #include "plibStdType.h"
 
-enum plibStdBST_STATUS { plibStdBST_status_LESS , plibStdBST_status_GREAT , plibStdBST_status_EQUAL } ;
+enum plibStdBST_Status { plibStdBST_status_LESS , plibStdBST_status_GREAT , plibStdBST_status_EQUAL } ;
 
 struct plibStdBST_Node
 {
@@ -15,7 +15,6 @@ struct plibStdBST_Node
 	struct plibStdBST_Node *Left , *Right ;
 } ;
 
-struct plibStdBST_Node* plibStdBST_search( struct plibStdBST_Node *RootNode , void *Key , enum plibStdBST_STATUS ( *judge )( void *Key1 , void *Key2 ) ) ;
+struct plibStdBST_Node* plibStdBST_search( struct plibStdBST_Node *RootNode , void *Key , enum plibStdBST_Status ( *judge )( void *Key1 , void *Key2 ) ) ;
 
-bool plibStdBST_insert( struct plibStdBST_Node **RootNode , struct plibStdBST_Node *NewNode , enum plibStdBST_STATUS ( *judge )( void *Key1 , void *Key2 ) ) ;
-struct plibStdBST_Node* plibStdBST_desert( struct plibStdBST_Node **RootNode , char *Key , enum plibStdBST_STATUS ( *judge )( void *Key1 , void *Key2 ) ) ;
+bool plibStdBST_insert( struct plibStdBST_Node **RootNode , struct plibStdBST_Node *NewNode , enum plibStdBST_Status ( *judge )( void *Key1 , void *Key2 ) ) ;
