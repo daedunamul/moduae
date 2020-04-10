437b5f766f1c3b110851f76fb0589c3a108ceb94
puppysss <49893731+puppysss@users.noreply.github.com>
2020-04-10 18:30:18 +0900

    Create plibStdDataBST.h

---
+++ plib/Std/Data/plibStdDataBST.h
@@ -0,0 +1,36 @@
+/*
+	plib::Std::Data::BST
+*/
+#pragma once
+#include "plibStdData.h"
+
+enum plibStdDataBST_Status { plibStdDataBST_Status_Less , plibStdDataBST_Status_Great , plibStdDataBST_Status_Equal } ;
+
+struct plibStdDataBST
+{
+	void *Key ;
+	void *Value ;
+	struct plibStdDataBST *Left , *Right ;
+} ;
+
+bool
+plibStdDataBST_push
+(
+	struct plibStdDataBST **ThisNode , 
+	struct plibStdDataBST *NewNode , 
+	enum plibStdDataBST_Status ( *Operator )( void *Key1 , void *Key2 ) 
+) ;
+struct plibStdDataBST*
+plibStdDataBST_pop
+(
+	struct plibStdDataBST **RootNode , 
+	void *Key , 
+	enum plibStdDataBST_Status ( *Operator )( void *Key1 , void *Key2 ) 
+) ;
+struct plibStdDataBST*
+plibStdDataBST_lookup
+(
+	struct plibStdDataBST *ThisNode , 
+	void *Key , 
+	enum plibStdDataBST_Status ( *Operator )( void *Key1 , void *Key2 ) 
+) ;
