d9862a526b25e4b6d8de54b00dab4d50fb36bd26
puppysss <49893731+puppysss@users.noreply.github.com>
2020-04-10 18:28:30 +0900

    Create plibStdDataChain.h

---
+++ plib/Std/Data/plibStdDataChain.h
@@ -0,0 +1,15 @@
+/*
+	plib::Std::Data::Chain
+*/
+#pragma once
+#include "plibStdData.h"
+
+struct plibStdDataChain
+{
+	struct plibStdDataChain *Left , *Right ;
+	void *Value ;
+} ;
+
+void plibStdDataChain_push( bool Direction , struct plibStdDataChain **TopNode , struct plibStdDataChain *NewNode ) ;
+struct plibStdDataChain* plibStdDataChain_pop( bool Direction , struct plibStdDataChain **TopNode ) ;
+struct plibStdDataChain* plibStdDataChain_lookup( bool Direction , unsigned int Index , struct plibStdDataChain *EntryNode ) ;
