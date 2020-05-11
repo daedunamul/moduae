451aff5e37ac505437a5cf2f685c376769b813ef
puppysss <49893731+puppysss@users.noreply.github.com>
2020-05-11 14:32:55 +0900

    Update plibStdDataChain.h

--- plib/Std/Data/plibStdDataChain.h
+++ plib/Std/Data/plibStdDataChain.h
@@ -12,4 +12,4 @@ struct plibStdDataChain
 
 void plibStdDataChain_push( bool Direction , struct plibStdDataChain **EntryNode , struct plibStdDataChain *NewNode ) ;
 struct plibStdDataChain* plibStdDataChain_pop( bool Direction , struct plibStdDataChain **EntryNode ) ;
-struct plibStdDataChain* plibStdDataChain_lookup( bool Direction , unsigned int Index , struct plibStdDataChain *EntryNode ) ;
+struct plibStdDataChain* plibStdDataChain_lookup( bool Direction , unsigned int Index , struct plibStdDataChain *ThisNode ) ;
