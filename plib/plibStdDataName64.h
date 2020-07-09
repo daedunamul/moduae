85930fc68dc8c905cd8f431a2497aa817b54860d
puppysss <49893731+puppysss@users.noreply.github.com>
2020-07-09 17:49:29 +0900

    Update and rename plibStdDataName64.h to plibStdFormatName64.h

--- plib/plibStdDataName64.h
+++
@@ -1,7 +0,0 @@
-/*
-	plib::Std::Data::Name64
-*/
-#pragma once
-#include "plibStdData.h"
-
-uint8_t plibStdDataName64_translateFromAscii( char Ascii ) ;
---
+++ plib/plibStdFormatName64.h
@@ -0,0 +1,7 @@
+/*
+	plib::Std::Format::Name64
+*/
+#pragma once
+#include "plibStdType.h"
+
+uint8_t plibStdFormatName64_translateFromAscii( char Ascii ) ;
