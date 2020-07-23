e82c2d53ff4bb8dec1f9f65cad05a6eda5724b29
puppysss <49893731+puppysss@users.noreply.github.com>
2020-07-24 07:35:22 +0900

    Update plibStdMemoryPool.h

--- plib/plibStdMemoryPool.h
+++ plib/plibStdMemoryPool.h
@@ -6,12 +6,15 @@
 
 struct plibStdMemoryPool
 {
-	size_t UnitSize , UsageCount , MaxCount ;
-	uint8_t *Data ;
+	size_t UnitSize , Count , MaxCount ;
+	uint8_t *AddressStart , *AddressEnd ;
+	uint8_t **AddressStack ;
 } ;
+
+void plbStdMemoryPool_initialize( struct plibStdMemoryPool *Pool ) ;
 
 struct plibStdMemoryPool* plibStdMemoryPool_createPool( size_t UnitSize , size_t MaxCount ) ;
 bool plibStdMemoryPool_deletePool( struct plibStdMemoryPool **Pool ) ;
 
 uint8_t* plbStdMemoryPool_allocate( struct plibStdMemoryPool *Pool ) ;
-void plbStdMemoryPool_flush( struct plibStdMemoryPool *Pool ) ;
+void plbStdMemoryPool_deallocate( struct plibStdMemoryPool *Pool , uint8_t **UsedAddress ) ;
