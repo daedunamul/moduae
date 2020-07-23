702148162b88a353ddee2db5edc9120ba15dd535
puppysss <49893731+puppysss@users.noreply.github.com>
2020-07-24 07:36:11 +0900

    Update plibStdMemoryPool.c

--- plib/plibStdMemoryPool.c
+++ plib/plibStdMemoryPool.c
@@ -3,17 +3,34 @@
 */
 #include "plibStdMemoryPool.h"
 
+void plbStdMemoryPool_initialize( struct plibStdMemoryPool *Pool )
+{
+	if( Pool == plibStdTypeNullPointer )
+		return ;
+		
+	uint8_t *Address ;
+	
+	for( Pool->Count = 0 ; Pool->Count < Pool->MaxCount ; Pool->Count ++ )
+	{
+		Address = Pool->AddressStart + ( 1 + Pool->UnitSize ) * Pool->Count ;
+		*Address = false ;
+		Pool->AddressStack[ Pool->Count ] = Address ;
+	}
+}
+
 struct plibStdMemoryPool* plibStdMemoryPool_createPool( size_t UnitSize , size_t MaxCount )
 {
 	if( UnitSize == 0 || MaxCount == 0 )
 		return plibStdTypeNullPointer ;
 		
 	struct plibStdMemoryPool *NewPool = ( struct plibStdMemoryPool* )malloc( sizeof( struct plibStdMemoryPool ) ) ;
-	
+
 	NewPool->UnitSize = UnitSize ;
 	NewPool->MaxCount = MaxCount ;
-	NewPool->UsageCount = 0 ;
-	NewPool->Data = ( uint8_t* )malloc( UnitSize * MaxCount ) ;
+	NewPool->AddressStart = ( uint8_t* )malloc( ( 1 + UnitSize ) * MaxCount ) ;
+	NewPool->AddressEnd = NewPool->AddressStart + ( 1 + UnitSize ) * ( MaxCount - 1 ) ;
+	NewPool->AddressStack = ( uint8_t** )malloc( sizeof( uint8_t* ) * MaxCount ) ;
+	plbStdMemoryPool_initialize( NewPool ) ;
 	
 	return NewPool ;
 }
@@ -22,7 +39,8 @@ bool plibStdMemoryPool_deletePool( struct plibStdMemoryPool **Pool )
 	if( *Pool == plibStdTypeNullPointer )
 		return false ;
 		
-	free( ( *Pool )->Data ) ;
+	free( ( *Pool )->AddressStack ) ;
+	free( ( *Pool )->AddressStart ) ;
 	free( *Pool ) ;
 	*Pool = plibStdTypeNullPointer ;
 	
@@ -31,16 +49,28 @@ bool plibStdMemoryPool_deletePool( struct plibStdMemoryPool **Pool )
 
 uint8_t* plbStdMemoryPool_allocate( struct plibStdMemoryPool *Pool )
 {
-	if( Pool == plibStdTypeNullPointer || Pool->UsageCount == Pool->MaxCount )
+	if( Pool == plibStdTypeNullPointer || Pool->Count == 0 )
 		return plibStdTypeNullPointer ;
-	Pool->UsageCount ++ ;
+		
+	Pool->Count -- ;
+	Pool->AddressStack[ Pool->Count ][ 0 ] = true ;
 	
-	return Pool->Data + ( Pool->UsageCount - 1 ) * Pool->UnitSize ;
+	return Pool->AddressStack[ Pool->Count ] + 1 ;
 }
-void plbStdMemoryPool_flush( struct plibStdMemoryPool *Pool )
+void plbStdMemoryPool_deallocate( struct plibStdMemoryPool *Pool , uint8_t **UsedAddress )
 {
-	if( Pool == plibStdTypeNullPointer )
+	if
+	(
+		Pool == plibStdTypeNullPointer
+		||
+		*UsedAddress < Pool->AddressStart || *UsedAddress > Pool->AddressEnd
+		||
+		*( *UsedAddress - 1 ) == false
+	)
 		return ;
-	
-	Pool->UsageCount = 0 ;
+		
+	*( *UsedAddress - 1 ) = false ;
+	Pool->AddressStack[ Pool->Count ] = *UsedAddress ;
+	Pool->Count ++ ;
+	*UsedAddress = plibStdTypeNullPointer ;
 }
