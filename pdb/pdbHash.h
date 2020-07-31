562af7da7d47c5dc1bb416858bf5741b8bee9557
puppysss <49893731+puppysss@users.noreply.github.com>
2020-07-31 17:40:34 +0900

    Delete pdbHash.h

--- pdb/pdbHash.h
+++
@@ -1,9 +0,0 @@
-/*
-	pdb::Hash
-*/
-// 임시로 사용할 예정 ... 나중에 plibCrypto로 이동
-#pragma once
-#include "../plib/plibStdData.h"
-
-uint8_t pdbHash_getBase64( char Ascii ) ;
-uint32_t pdbHash_getAdler32( uint8_t *Data ) ;
