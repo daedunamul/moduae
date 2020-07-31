1f9c96dcb3dcc97bf0ed734b576e817a139ca9c0
puppysss <49893731+puppysss@users.noreply.github.com>
2020-07-31 17:43:32 +0900

    Create emptydbDB.c

---
+++ emptydb/emptydbDB.c
@@ -0,0 +1,47 @@
+/*
+	emptydb::DB
+*/
+#include "emptydbDB.h"
+
+struct emptydbDB* emptydbDB_createDB( size_t ObjectMaxCount , size_t KeyValueMaxCount )
+{
+	if( ObjectMaxCount < 0 || KeyValueMaxCount < 0 )
+		return plibStdTypeNullPointer ;
+		
+	struct emptydbDB *NewDB = ( struct emptydbDB* )malloc( sizeof( struct emptydbDB ) ) ;
+	
+	ObjectMaxCount ++ ; // Root Object
+	
+	NewDB->ObjectMaxCount = ObjectMaxCount ;
+	NewDB->ObjectCount = 0 ;
+	NewDB->KeyValueMaxCount = KeyValueMaxCount ;
+	NewDB->KeyValueCount = 0 ;
+	NewDB->ObjectRootNode = plibStdTypeNullPointer ;
+	NewDB->ObjectThisNode = plibStdTypeNullPointer ;
+	NewDB->ObjectNodePool = plibStdMemoryPool_createPool
+	(
+		sizeof( struct emptydbDB ) + sizeof( emptydbDBKeyType ) + sizeof( struct emptydbDBObjectValueType ) ,
+		ObjectMaxCount
+	) ;
+	NewDB->KeyValueNodePool = plibStdMemoryPool_createPool
+	( 
+		sizeof( struct emptydbDB ) + sizeof( emptydbDBKeyType ) ,
+		KeyValueMaxCount
+	) ;
+	
+	return NewDB ;
+}
+bool emptydbDB_deleteDB( struct emptydbDB **DB )
+{
+	if( *DB == plibStdTypeNullPointer )
+		return false ;
+	
+	if( ( *DB )->KeyValueNodePool == plibStdTypeNullPointer )
+		plibStdMemoryPool_deletePool( &( *DB )->KeyValueNodePool ) ;
+	if( ( *DB )->ObjectNodePool == plibStdTypeNullPointer )
+		plibStdMemoryPool_deletePool( &( *DB )->ObjectNodePool ) ;
+	free( *DB ) ;
+	*DB = plibStdTypeNullPointer ;
+	
+	return true ;
+}
