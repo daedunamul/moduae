ac0766ec0f76f9774fe0589e33eeb56094451661
puppysss <49893731+puppysss@users.noreply.github.com>
2020-08-19 14:25:51 +0900

    Update emptydbKeyValue.h

--- emptydb/emptydbKeyValue.h
+++ emptydb/emptydbKeyValue.h
@@ -6,6 +6,6 @@
 
 bool emptydbKeyValue_createKeyValue( struct emptydbRoot *Root , size_t KeyCount , emptydbCommonKeyType *KeyArray , size_t DataSize , size_t DataLength ) ;
 bool emptydbKeyValue_deleteKeyValue( struct emptydbRoot *Root , size_t KeyCount , emptydbCommonKeyType *KeyArray ) ;
-void emptydbKeyValue_flushKeyValue( struct emptydbRoot *Root , struct plibStdDataBST *ObjectRootNode ) ;
+void emptydbKeyValue_flushKeyValue( struct emptydbRoot *Root , struct plibStdDataBST *KeyValueRootNode ) ;
 
 size_t emptydbKeyValue_lookupKeyValue( struct emptydbRoot *Root , size_t KeyCount , emptydbCommonKeyType *KeyArray , struct plibStdDataBST **ResultKeyValueArray ) ;
